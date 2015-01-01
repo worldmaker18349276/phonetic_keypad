2d5d522fe598a366ee505290bea44d12adbad183
worldmaker <worldmaker18349276@gmail.com>
2015-01-01 17:13:57 +0800

    reorganize text.js & rename input.js->control.js

--- js/control.js
+++ js/control.js
@@ -1,3 +1,6 @@
+var current_index = 0;
+
+
 function keyin( button ) {
 	var phone = button.innerHTML;
 	total_cchar[current_index].putPhone(phone);
@@ -18,3 +21,12 @@ function space() {
 function sel() {
 	alert(total_cchar[current_index].text + ': ' + total_cchar[current_index].phones.filter(String).join(' '));
 }
+
+function text_del() {
+	total_cchar.splice(current_index, 1);
+}
+
+function text_insert( cchar ) {
+	total_cchar.splice(current_index, 0, cchar);
+}
+
--- js/print.js
+++ js/print.js
@@ -1,3 +1,48 @@
+var canvas;
+var text_size; /*text height (px)*/
+var line_spacing;
+
+var total_cchar = new Array();
+var hightlight = new Array();
+
+
+function repaintText() {
+	canvas.width = canvas.width;
+	var cx = canvas.getContext('2d');
+
+	cx.save();
+	cx.translate(0, text_size/2 + line_spacing/2);
+
+	cx.save();
+	var offset = 0;
+	for ( var i=0; i<=total_cchar.length; i++ ) {
+		var cchar_width = measureCChar(cx, text_size, total_cchar[i]);
+		offset += cchar_width;
+		if ( offset > canvas.width ) {
+			cx.restore();
+			cx.translate(0, text_size+line_spacing);
+			cx.save();
+			offset = cchar_width;
+		}
+		if ( hightlight[i] == 'warning' )
+			printBackGround(cx, text_size, 'Red');
+		else if ( hightlight[i] == 'edit' )
+			printBackGround(cx, text_size, 'LightGreen');
+		else if ( hightlight[i] == 'select' )
+			printBackGround(cx, text_size, 'Blue');
+		else if ( hightlight[i] == 'focus' )
+			printCursor(cx, text_size, 'Black');
+
+		if ( total_cchar[i] )
+			printCChar(cx, text_size, total_cchar[i]);
+		cx.translate(cchar_width, 0);
+	}
+	cx.restore();
+
+	cx.restore();
+}
+
+
 function measureCChar( cx, size, cchar ) {
 	var total_width = 0;
 	cx.save();
